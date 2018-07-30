d90c93a5c9fccb0727d556cdc6da754b75927bd1
aa shulpin <aashulpin@gmail.com>
2018-07-30 07:29:50 +0500

    added recycler view with some test data

---
+++ app/src/main/java/ru/you11/alarmclock/AlarmsListActivity.kt
@@ -0,0 +1,69 @@
+package ru.you11.alarmclock
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class AlarmsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+
+        //testData
+        val alarmNames = createFakeDataForAlarm()
+
+        val rvManager = LinearLayoutManager(this)
+        val rvAdapter = AlarmsRWAdapter(alarmNames)
+        val recyclerView = findViewById<RecyclerView>(R.id.all_alarms_recycler_view).apply {
+            layoutManager = rvManager
+            adapter = rvAdapter
+        }
+    }
+
+    private fun createFakeDataForAlarm(): ArrayList<String> {
+        val alarmNames = ArrayList<String>()
+        alarmNames.add("alarm")
+        alarmNames.add("alarm2")
+        alarmNames.add("alarm3")
+
+        return alarmNames
+    }
+}
+
+
+class AlarmsRWAdapter(private val alarmNames: ArrayList<String>): RecyclerView.Adapter<AlarmsRWAdapter.ViewHolder> () {
+
+
+    class ViewHolder(val layout: LinearLayout) : RecyclerView.ViewHolder(layout) {
+        val alarmName: TextView = layout.findViewById(R.id.alarm_name_text_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlarmsRWAdapter.ViewHolder {
+
+        val layout = LayoutInflater.from(parent.context)
+                .inflate(R.layout.alarm_recycler_view_card, parent, false) as LinearLayout
+
+        return ViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.alarmName.text = alarmNames[position]
+    }
+
+    override fun getItemCount(): Int {
+        return alarmNames.size
+    }
+
+
+}
