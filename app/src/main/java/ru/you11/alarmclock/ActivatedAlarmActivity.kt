acb4fd2c9fbd06f05c8b6f28f4e7b64fe37ac2cd
aa shulpin <aashulpin@gmail.com>
2018-08-04 21:57:29 +0500

    dismiss button works

--- app/src/main/java/ru/you11/alarmclock/ActivatedAlarmActivity.kt
+++ app/src/main/java/ru/you11/alarmclock/ActivatedAlarmActivity.kt
@@ -1,27 +1,28 @@
 package ru.you11.alarmclock
 
-import android.app.Activity
 import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Build
-import android.os.Build.VERSION.SDK
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.MediaController
+import android.widget.Button
 import android.widget.Toast
 
-class AlarmStartActivity: AppCompatActivity() {
+class ActivatedAlarmActivity: AppCompatActivity() {
+
+    val mediaPlayer = MediaPlayer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alarm_start)
+        setContentView(R.layout.activity_activated_alarm)
 
         setupMediaPlayer()
+        setupDelayButton()
+        setupDismissButton()
     }
 
     private fun setupMediaPlayer() {
-        val mediaPlayer = MediaPlayer()
         if (Build.VERSION.SDK_INT < 21) {
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM)
         } else {
@@ -33,11 +34,28 @@ class AlarmStartActivity: AppCompatActivity() {
         }
         val url = "https://d1u5p3l4wpay3k.cloudfront.net/dota2_gamepedia/5/58/Pain_pain_17.mp3"
         mediaPlayer.setDataSource(url)
-        mediaPlayer.setVolume(50f, 50f)
         mediaPlayer.prepareAsync()
         mediaPlayer.setOnPreparedListener {
             Toast.makeText(this, "prepared!", Toast.LENGTH_SHORT).show()
             mediaPlayer.start()
         }
+    }
+
+    private fun setupDelayButton() {
+
+    }
+
+    private fun setupDismissButton() {
+        findViewById<Button>(R.id.activated_alarm_dismiss_button).apply {
+            setOnClickListener {
+                mediaPlayer.stop()
+                finish()
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaPlayer.stop()
     }
 }
--- app/src/main/java/ru/you11/alarmclock/AlarmReceiver.kt
+++ app/src/main/java/ru/you11/alarmclock/AlarmReceiver.kt
@@ -1,20 +1,15 @@
 package ru.you11.alarmclock
 
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.media.RingtoneManager
-import android.support.v4.app.NotificationCompat
 import android.util.Log
-import android.widget.Toast
 
 class AlarmReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
 
-        context?.startActivity(Intent(context, AlarmStartActivity::class.java))
+        context?.startActivity(Intent(context, ActivatedAlarmActivity::class.java))
         Log.d("alarmTesting", "alarm started")
     }
 }
