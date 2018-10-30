1725c22ec7fce54bdaee55c9a767950f7aefaad1
aa shulpin <aashulpin@gmail.com>
2018-10-30 16:30:32 +0500

    spelling error

--- app/src/main/java/ru/you11/alarmclock/MainApp.kt
+++ app/src/main/java/ru/you11/alarmclock/MainApp.kt
@@ -18,11 +18,4 @@ class MainApp: Application() {
         }
     }
 
-    override fun onCreate() {
-        super.onCreate()
-//        if (LeakCanary.isInAnalyzerProcess(this)) {
-//            return
-//        }
-//        LeakCanary.install(this)
-    }
 }
